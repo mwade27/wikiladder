212466b5075375b4ccd878499a1cd112a14cf1ca
Maxwell Wade <mew2@uab.edu>
2024-12-01 20:19:02 -0600

    moving to pc

---
+++ FinalProject/main.cpp
@@ -0,0 +1,22 @@
+#include "wikiladder.h"
+#include <iostream>
+
+
+
+int main(){
+
+
+    std::string startPage, endPage;
+
+
+    std::cout << "Enter a starting page: " << std::endl;
+
+    std::cin >> startPage;
+
+    std::cout << "Enter a end page: " << std::endl;
+
+    std::cin >> endPage;
+
+
+    
+}
---
+++ FinalProject/wikiladder.h
@@ -0,0 +1,19 @@
+#ifndef WIKILADDER_H
+#define WIKILADDER_H
+
+class WikiLadder{
+
+    public:
+
+
+
+
+    private:
+
+
+};
+
+
+
+
+#endif
